920ae4f7fb024607cbaef88d09e2cdf45e5aebab
Ali <mroueh_ali@outlook.com>
2023-05-31 15:08:32 +0300

    cors policy

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from "path";
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
  
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
+app.use(cors());
 app.use("/public", express.static(path.join(__dirname, "../esa-applicants-data/onlineUsers")));
 
 app.get('/', (req, res) =>{
